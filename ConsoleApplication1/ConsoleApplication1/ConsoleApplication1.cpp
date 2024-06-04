051c9ed5cacf4cbaf5026b409c2e949153f9f80c
kaaw13 <156587876+kaaw13@users.noreply.github.com>
2024-06-04 15:52:50 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+
+
+
+	//Window
+	sf::Window window(sf::VideoMode(640, 480), "Moja pierwsza gra", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event event;
+	class Gra {
+	private:
+		sf::Window window;
+		sf::Event event;
+
+	public:
+		Gra() : window(sf::VideoMode(640, 480), "Moja pierwsza gra", sf::Style::Titlebar | sf::Style::Close) {}
+
+		void run() {
+			//Game loop
+			while (window.isOpen()) {
+				//Event polling
+				while (window.pollEvent(event)) {
+					switch (event.type) {
+					case sf::Event::Closed:
+						window.close();
+						break;
+					case sf::Event::KeyPressed:
+						if (event.key.code == sf::Keyboard::Escape) {
+							window.close();
+						}
+						break;
+					}
+				}
+				//Update
+
+				//Render
+				 //czyszczenie okna
+				window.display(); //wyświetlanie
+			}
+		}
+	};
+
+	
+	int main() {
+		Gra gra;
+		gra.run();
+		return 0;
+	}
---
+++ ConsoleApplication1/ConsoleApplication1/zad dom 2.cpp
@@ -0,0 +1,58 @@
+﻿#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+   
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Ruszajace sie kolo");
+
+    
+    sf::CircleShape circle(30);
+    circle.setFillColor(sf::Color::Blue);
+
+   
+    float posX = static_cast<float>(std::rand() % (window.getSize().x - 2 * static_cast<int>(circle.getRadius())) + circle.getRadius());
+    float posY = static_cast<float>(std::rand() % (window.getSize().y - 2 * static_cast<int>(circle.getRadius())) + circle.getRadius());
+    circle.setPosition(posX, posY);
+
+   
+    float speedX = static_cast<float>(std::rand() % 50 - 10); 
+    float speedY = static_cast<float>(std::rand() % 50 - 10);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        
+        posX += speedX * 0.01f; 
+        posY += speedY * 0.01f;
+
+       
+        if (posX + circle.getRadius()+2 >= window.getSize().x || posX  <= 0)
+            speedX = -speedX; 
+        if (posY + circle.getRadius()+2 >= window.getSize().y || posY  <= 0)
+            speedY = -speedY; 
+
+       
+        circle.setPosition(posX, posY);
+
+        
+        window.clear();
+
+       
+        window.draw(circle);
+
+        window.display();
+    }
+
+    return 0;
+}
